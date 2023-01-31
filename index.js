035b4dd5a5c636a28a1e611272b93e2b3f145258
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2023-01-31 19:03:30 +0600

    added all api route for CRUD

--- index.js
+++ index.js
@@ -9,6 +9,69 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zuoxzfe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const blogsCollection = client.db('skBlog').collection('blogs');
+
+    /* ------ 📝Blogs📝 ------- */
+    // get all Blogs
+    app.get('/blogs', async (req, res) => {
+      const query = {}
+      const blogs = await blogsCollection.find(query).toArray()
+
+      res.send(blogs)
+    })
+
+    // get a blog by id
+    app.get('/blogs/:id', async (req, res) => {
+      const { id } = req.params
+      const query = { _id: ObjectId(id) }
+
+      const blog = await blogsCollection.findOne(query)
+      res.send(blog)
+    })
+
+    // create a new blog
+    app.post('/blogs', async (req, res) => {
+      const blog = req.body
+      const result = await blogsCollection.insertOne(blog)
+
+      res.send(result)
+    })
+
+    // update an blog by id
+    app.patch('/blogs/:id', async (req, res) => {
+      const { id } = req.params
+      const updateInfo = req.body
+
+      const query = { _id: ObjectId(id) }
+      const updatedDoc = {
+        $set: updateInfo
+      }
+      const result = await blogsCollection.updateOne(query, updatedDoc)
+
+      res.send(result)
+    })
+
+    // delete an blog by id
+    app.delete('/blogs/:id', async (req, res) => {
+      const { id } = req.params
+      const query = { _id: ObjectId(id) }
+
+      const result = await blogsCollection.deleteOne(query)
+
+      res.send(result)
+    })
+
+  } finally { }
+}
+
+run().catch(err => console.error(err))
+
+
 // Default Route
 app.get("/", (req, res) => {
   res.send("SK Blog Server is running")
